eb26656e3038929e988afef21d11012984df4f80
Max Cuevas <maximilianocuevas777@gmail.com>
2019-11-10 20:39:16 -0800

    Add textureprogress bar for health

--- Main.gd
+++ Main.gd
@@ -4,6 +4,8 @@ var enemy = preload("res://enemy.tscn")
 func _on_EnemyRespawnTimer_timeout():
 	var newEnemy = enemy.instance()
 	add_child(newEnemy)
+	print($TextureProgress.value)
+	$TextureProgress.value-=10
 
 func _on_Player_death():
 	get_tree().paused = true
